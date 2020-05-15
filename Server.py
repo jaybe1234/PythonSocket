958fa32cbb9c3830254370d8444373c02080815b
justinunited <71zf@99e>
2020-05-16 04:55:37 +0700

    fixed bug

--- Server.py
+++ Server.py
@@ -5,8 +5,8 @@ from msgHandler import msgHandler
 class Server:
     def __init__(self, port):
         self.ip = "35.240.204.36"#socket.gethostbyname(socket.gethostname())
-        print(self.ip, self.port)
         self.port = port
+        print(self.ip, self.port)
         self.userNum = 0
         self.Server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
