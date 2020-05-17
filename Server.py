9d0e3baf349de533a307d102276463410f562f02
justinunited <71zf@99e>
2020-05-18 01:53:27 +0700

    debug

--- Server.py
+++ Server.py
@@ -38,7 +38,7 @@ class Server:
                 print(f"[EXCEPTION]", e)
                 break
         for i in range(len(self._msghandler.connectionList)):
-            print(conn == self._msghandler.connectionList[i][0])
+            print((conn, addr) == self._msghandler.connectionList[i])
         self._msghandler.removeUser(conn,addr)
         conn.close()
         self.userNum -= 1
